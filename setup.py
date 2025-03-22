d0ff478c8ba082785621a5e8cd989217e0c5ad0b
chenhui.lux.yu <chenhui.lux.yu@outlook.com>
2025-03-22 01:42:55 +0000

    Update gitignore.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setuptools.setup(
     name="cmake_node_editor",
     version="0.1.0",
     packages=["cmake_node_editor"],
-    install_requires=["PyQt6"],  # 需要先安装PyQt6
+    install_requires=["PyQt6"],
     entry_points={
         "console_scripts": [
             "cmake-node-editor = cmake_node_editor.main:main",  
